7a93f61cf13fef2a5673e4c3a9693b9d820c77e3
tancredofernando <tancredofernandoneves@gmail.com>
2017-01-05 14:19:58 -0300

    Adicionar arquivos de projeto.

---
+++ LinqExcel/CaseTest.cs
@@ -0,0 +1,27 @@
+﻿using LinqToExcel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExcel
+{
+   public class CaseTest
+    {
+       // [ExcelColumn("FOLDER")]
+       // public string FOLDER { get; set; }
+       // [ExcelColumn("SUITE")]
+       // public string SUITE { get; set; }
+        [ExcelColumn("NOME DO CASO DE TESTE")]
+        public string NOMEDOTESTE { get; set; }
+        [ExcelColumn("PRÉ-CONDIÇÃO")]
+        public string PRECONDICAO { get; set; }
+        [ExcelColumn("ETAPA")]
+        public string ETAPA { get; set; }
+        [ExcelColumn("DESCRIÇÃO")]
+        public string DESCRICAO { get; set; }
+        [ExcelColumn("RESULTADO ESPERADO")]
+        public string RESULTADOESPERADO { get; set; }
+    }
+}
---
+++ LinqExcel/Program.cs
@@ -0,0 +1,80 @@
+﻿using LinqToExcel;
+using LinqToExcel.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LinqExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToExcelFile = "" + @"C:\Users\LGMONEZI\Desktop\testlink\Cadastro de Proposta.xls";
+            string sheetName = "Query1";
+            var excelFile = new ExcelQueryFactory(pathToExcelFile);
+            var casetest = from a in excelFile.Worksheet<CaseTest>(sheetName) select a;
+
+            //foreach (var a in casetest)
+            //{
+            //    string Info = " NOMEDOTESTE: {0}; PRECONDICAO: {1}; ETAPA: {2}; DESCRICAO: {3};RESULTADOESPERADO: {4}";
+            //    Console.WriteLine(Info, a.NOMEDOTESTE, a.PRECONDICAO, a.ETAPA, a.DESCRICAO, a.RESULTADOESPERADO);
+            //}
+            // Console.ReadKey();
+
+            //pega a quantidade de caso de test
+            string[] linhas = casetest.Select(t => t.NOMEDOTESTE).Take(2).ToArray();
+            string[] pre = casetest.Select(t => t.NOMEDOTESTE).Take(2).ToArray();
+            string[] etapa = casetest.Select(t => t.ETAPA.Replace("Etapa ", "")).Take(2).ToArray();
+            string[] des = casetest.Select(t => t.DESCRICAO).Take(2).ToArray();
+            string[] res = casetest.Select(t => t.RESULTADOESPERADO).Take(2).ToArray();
+
+            //Cria o elemento raiz do XML
+            var xsurvey = new XDocument(new XDeclaration("1.0", "UTF-8", "No"));
+            var xtestcases = new XElement("testcases");
+            //percorre cada linha do arquivo casetest
+            for (int i = 0; i < linhas.Length; i++)
+            {
+                //Cria cada linha
+                if (i > 0)
+                {
+                    xtestcases.Add(gera_Linha_XML(linhas[i], linhas[0], pre[i], etapa[i], des[i], res[i]));
+                }
+            }
+            xsurvey.Add(xtestcases);
+            //xsurvey.Save(Console.Out); Console.WriteLine(); Console.ReadKey();
+            xsurvey.Save(@"C:\Users\LGMONEZI\Desktop\testlink\Caso.xml");
+        }
+
+        private static XElement gera_Linha_XML(string linha, string primeiraLinha, string pre, string etapa, string des, string res)
+        {
+            //separa o nome dos campos do arquivo CSV
+            string nomes = linha;
+            string preconditions = pre;
+            string steps = etapa;
+            string actions = des;
+            string expectedresults = res;
+            string internalid = "";
+            //Cria o elemento var e os atributos com o nome do campo e valor
+            var xmlLinha = new XElement("testcase", new XAttribute("internalid", internalid), new XAttribute("name", nomes),
+                                        new XElement("node_order", new XCData("")),
+                                        new XElement("externalid", new XCData("")),
+                                        new XElement("version", new XCData("")),
+                                        new XElement("summary", new XCData("")),
+                                        new XElement("preconditions", new XCData(preconditions)),
+                                        new XElement("execution_type", new XCData("1")),
+                                        new XElement("importance", new XCData("2")),
+                                        new XElement("steps", new XElement("step", new XElement("step_number", new XCData(steps)),
+                                                                                new XElement("actions", new XCData(actions)),
+                                                                                new XElement("expectedresults", new XCData(expectedresults)),
+                                                                                new XElement("execution_type", new XCData("1")))));
+
+            return xmlLinha;
+        }
+    }
+}
+
